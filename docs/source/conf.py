505ec8e90b7d946ff5813e1caa172b38346371c9
Stefan Wahren <stefan.wahren@chargebyte.com>
2024-12-19 09:35:43 +0100

    Update copyright
    Signed-off-by: Stefan Wahren <stefan.wahren@chargebyte.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Charge SOM User Guide'
-copyright = '2024, chargebyte GmbH'
+copyright = '2025, chargebyte GmbH'
 author = 'chargebyte GmbH'
 
 # -- General configuration ---------------------------------------------------
