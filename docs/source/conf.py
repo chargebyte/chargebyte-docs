3e986ca4c72ef3baadaefeb27d71d1567763d1c6
Michael Heimpold <michael.heimpold@chargebyte.com>
2025-03-31 13:04:48 +0200

    Sync the config with other branches
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,23 +14,29 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['linuxdoc.rstFlatTable', 'sphinx_copybutton']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+numfig = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_title = project
 html_theme = 'classic'
 html_static_path = ['_static']
 html_logo = '_static/cb_logo.png'
 html_css_files = [
     'css/cb_theme.css',
 ]
-html_show_sourcelink = False
+
+# Disable link "index"
 html_use_index = False
+
+# Disable link "Show Source"
+html_show_sourcelink = False
 
 # -- Options for the linkcheck builder ---------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-linkcheck-builder
