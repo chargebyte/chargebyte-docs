995bb8c910d10493306cd5d9fff9b6c05accbe5b
Michael Heimpold <michael.heimpold@chargebyte.com>
2025-11-05 07:13:27 +0100

    utils: dbc_to_rst.py: factor out configuration
    That will make it easier to sync the tool between various platforms.
    
    Signed-off-by: Michael Heimpold <michael.heimpold@chargebyte.com>

--- docs/source/utils/dbc_to_rst.py
+++ docs/source/utils/dbc_to_rst.py
@@ -1,20 +1,9 @@
 import cantools
 from cantools.subparsers.dump import formatting
+from dbc_to_rst_config import *
 
 # Load the DBC file
 db = cantools.database.load_file("uart_com.dbc")
-
-# Messages you want to document
-target_msgs = ["ChargeControl1", "ChargeState1", "PT1000State", "FirmwareVersion", "GitHash", "InquiryPacket"]
-
-# Sender name mapping
-sender_name_map = {
-    "Default_SafetyController": "Safety Controller",
-    "Default_Linux": "Linux Processor"
-}
-
-# Senders to exclude from display
-excluded_senders = {"CCY_SafetyController"}
 
 # Extract a signal row as a list of strings
 def signal_row(signal):
---
+++ docs/source/utils/dbc_to_rst_config.py
@@ -0,0 +1,11 @@
+# Messages you want to document
+target_msgs = ["ChargeControl1", "ChargeState1", "PT1000State", "FirmwareVersion", "GitHash", "InquiryPacket"]
+
+# Sender name mapping
+sender_name_map = {
+    "Default_SafetyController": "Safety Controller",
+    "Default_Linux": "Linux Processor"
+}
+
+# Senders to exclude from display
+excluded_senders = {"CCY_SafetyController"}
